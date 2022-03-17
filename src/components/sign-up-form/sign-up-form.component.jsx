d0aea849f795b7215558be3c7dc0a5785f4d0aa8
Stephen Moore <judo2000@comcast.net>
2022-03-17 17:45:20 -0600

    Install bootstrap and implement bootstrap navbar and create about me component and started my work component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
-import "./sign-up-form.styles.scss";
+import { UserContext } from "../../contexts/user.context";
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
 import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
@@ -21,6 +22,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log("Hit!");
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
