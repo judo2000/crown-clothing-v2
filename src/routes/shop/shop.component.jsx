d65868b8abdbf3c75c25e82ffc8a490beb23fc06
Stephen Moore <judo2000@comcast.net>
2022-03-20 16:39:52 -0600

    Added in the add to cart feature

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-cart.component";
+import ProductCard from "../../components/product-card/product-card.component";
 
 import "./shop.styles.scss";
 const Shop = () => {
